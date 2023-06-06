55ec35baaa4d33040fb24bb088bb6af234688e10
Akash A <akash.a.dckap@gmail.com>
2023-06-06 16:20:59 +0530

    combine_array

---
+++ find the array/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$finalOutput = [];
+
+$keys = array(
+    "field1"=>"first",
+    "field2"=>"second",
+    "field3"=>"third"
+);
+$values = array(
+    "field1value"=>"dinosaur",
+    "field2value"=>"pig",
+    "field3value"=>"platypus"
+);
+
+$keys1 = array_values($keys);
+$values1 = array_values($values);
+
+$combineArray = array_combine($keys1,$values1);
+print_r($combineArray);
